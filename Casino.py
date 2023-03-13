4da2077201f95552b896455f3a987200a6290235
Follow13 <timurdpasechnik@gmail.com>
2023-03-13 22:35:36 +0300

    осталось к основной программе прикрутить игры

---
+++ Casino.py
@@ -0,0 +1,17 @@
+import Casino_roulette
+
+def say_hi(): 
+    print('Приветствую в казино "7 Бананов"')
+
+def money():
+    your_money = int(input('Сколько у вас бабла '))
+
+def offer_play():
+    print('Казино')
+    print('Блекджек')
+    print('Джекпот')
+    offer = input('В какую игру хотите сыграть? ')
+    
+
+
+    
